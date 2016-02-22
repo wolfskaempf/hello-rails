11f344a34091ecfb424678cc6db5309434991834
wolfskaempf <tom@wolfskaempf.de>
2016-02-22 23:55:51 +0100

    add signup functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'articles#index'
 
+   get 'signup' => 'users#new'
+   resources :users
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
