304e2638248862e1607366b910084b724b46303a
wolfskaempf <tom@wolfskaempf.de>
2016-02-22 23:39:11 +0100

    generate users controller and migrate User model to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220152351) do
+ActiveRecord::Schema.define(version: 20160222223312) do
 
   create_table "articles", force: :cascade do |t|
     t.text     "body"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20160220152351) do
     t.boolean  "published"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
