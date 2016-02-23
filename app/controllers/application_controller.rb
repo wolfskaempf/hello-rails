deb8810ba1f9d0a86c27355bf713b32172d2290d
wolfskaempf <tom@wolfskaempf.de>
2016-02-23 12:00:36 +0100

    add authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,8 @@ class ApplicationController < ActionController::Base
     redirect_to '/login' unless current_user
   end
 
+  def require_author
+    redirect_to '/login' unless current_user.author?
+  end
+
 end
