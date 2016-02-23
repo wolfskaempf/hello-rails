480d6187790e8206e6a14e52d932f5c126f56eb2
wolfskaempf <tom@wolfskaempf.de>
2016-02-23 11:50:39 +0100

    add role to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  def author?
+    self.role == 'author'
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222223312) do
+ActiveRecord::Schema.define(version: 20160223104646) do
 
   create_table "articles", force: :cascade do |t|
     t.text     "body"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20160222223312) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "role"
   end
 
 end
