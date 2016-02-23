c2403b7774518aaacc6bdac38863f9b998b47d62
wolfskaempf <tom@wolfskaempf.de>
2016-02-23 11:56:39 +0100

    improve logout

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ Rails.application.routes.draw do
    get 'login' => 'sessions#new'
    post 'login' => 'sessions#create'
 
-   get 'logout' => 'sessions#destroy'
    delete 'logout' => 'sessions#destroy'
 
   # Example of regular route:
