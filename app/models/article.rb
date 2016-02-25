2801b9699ca242416ef3afd77b7ac8cfe60393ce
wolfskaempf <tom@wolfskaempf.de>
2016-02-25 15:39:21 +0100

    order articles backwards by standard

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true
   validates :body, presence: true
+
+  default_scope { order(created_at: :desc) } # We want our blog to show the newest article in front, so we order them in reverse by default
 end
--- spec/models/:article_spec.rb
+++ spec/models/:article_spec.rb
@@ -10,4 +10,11 @@ RSpec.describe :article, type: :model do
   it "is invalid without a body" do
     expect(FactoryGirl.build(:article, body: nil)).not_to be_valid
   end
+  context "2 or more" do
+    it "orders them in reverse chronologically" do
+      article1 = FactoryGirl.create(:article)
+      article2 = FactoryGirl.create(:article)
+      expect(Article.all).to eq([article2, article1])
+    end
+  end
 end
